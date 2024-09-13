0c19bea1d69ad24dc7b863176e761b178be4eba0
SimpPoseidon <120441879+TanishqMehrunkarIIPSDAVV@users.noreply.github.com>
2024-09-13 17:56:13 +0530

    student api

--- routes/Studentroutes.js
+++ routes/Studentroutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { studentlogin } = require("../controllers/StudentController");
+const {getStudentDetailsByStudentId} = require("../controllers/StudentController2");
 
 const router = express.Router();
 
 router.post("/login",studentlogin)
+router.post("/getStudentDetailsByStudentId",getStudentDetailsByStudentId);
 module.exports = router;
 
