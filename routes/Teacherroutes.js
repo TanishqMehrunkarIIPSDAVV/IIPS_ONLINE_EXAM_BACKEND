5a574a014a91cbcade749c7181d40e859bb67757
SimpPoseidon <120441879+TanishqMehrunkarIIPSDAVV@users.noreply.github.com>
2024-09-18 18:41:32 +0530

    change password

--- routes/Teacherroutes.js
+++ routes/Teacherroutes.js
@@ -7,6 +7,7 @@ const {
   verifyOtppasscode,
   forgotPassword,
   resetPassword,
+  changePassword,
 } = require("../controllers/TeacherController");
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.post("/verifypasscode", verifyOtppasscode);
 router.post("/verify-session", verifySession);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
+router.post("/change-password",changePassword);
 
 module.exports = router;
