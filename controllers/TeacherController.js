a84f6f1ddeb7020aa06e73b2c0fd606c09fc927d
Kartikey2011yadav <nykay10968@gmail.com>
2024-10-03 19:17:55 +0530

    backend patch: update teacher details by id

--- controllers/TeacherController.js
+++ controllers/TeacherController.js
@@ -6,6 +6,7 @@ const {
 } = require("../config/nodemailer");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
+const { log } = require("console");
 
 const signUp = async (req, res) => {
   const { name, email, mobileNumber, password } = req.body;
@@ -254,28 +255,30 @@ const resetPassword = async (req, res) => {
 
 
 const updateTeacherDetailsById = async (req, res) => {
-  const { _id, name, mobile, password } = req.body;
-
-  let updateFields = { name, mobile }; // Start with name and mobile
+  const { teacherId, name, mobile_no, password, email } = req.body;
+  console.log(req.body);
+  let updateFields = { name, mobile: mobile_no, email }; // Starting with name, email and mobile
 
   // Hash the password only if it's provided
   if (password) {
     const hashedPassword = await bcrypt.hash(password, 10);
     updateFields.password = hashedPassword;
   }
-
   try {
     const teacher = await Teacher.findOneAndUpdate(
-      { _id: _id },
+      { _id: teacherId },
       updateFields, // Only the fields that need to be updated
     );
+    console.log(updateFields);
 
     if (!teacher) {
+      console.log("teacher not found");
       return res.status(404).json({ error: "Teacher not found" });
     }
 
     res.status(200).json({ message: "Teacher updated successfully", teacher });
   } catch (error) {
+    console.log("catch");
     res.status(500).json({ error: "Server error" });
   }
 };
